3907740c845d88ce23e7807ec98ea41017dd2495
andrsavi <mr.andrew111@yandex.ru.ru>
2020-05-15 01:39:10 +0400

    Added Exercise.class

---
+++ src/po83/savinov/oop/model/Exercise.java
@@ -0,0 +1,24 @@
+package po83.savinov.oop.model;
+
+public class Exercise {
+    private  String name; //название упражнения
+    private  int duration; //продолжительность в минутах
+    public  static  final  int dur = 3; //время выполнения - упражнения 3 минуты
+
+    Exercise(String name) {
+        this.duration = dur;
+    }
+
+    Exercise(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public String getName() { return  name; } //метод, возвращающий название
+
+    public  void setName(String newName) { name = newName; } //метод, устанавливающий новое значение названия
+
+    public  int getDuration() { return  duration; } // метод, возвращающий продолжительность
+
+    public void setDuration(int newDuration) { duration = newDuration; } // метод, возвращающий новое значение продолжительности
+}
